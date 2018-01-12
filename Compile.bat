1eeaeb9303a25d88dd4a6a5554c5658a3d1e8b25
KeyJ148 <kejik148@gmail.com>
2018-01-12 11:49:11 +0300

    Убрана интерполяция как неэффективная Изменен файл компиляции из-за изменения названия пакета

--- Compile.bat
+++ Compile.bat
@@ -1,4 +1,4 @@
 rmdir /s/q bin
 mkdir bin
-javac -classpath ./lib/lwjgl.jar;./lib/slick-util.jar -sourcepath ./src -d ./bin -encoding UTF-8 ./src/engine/Loader.java ./src/engine/net/server/GameServer.java ./src/game/client/person/equipment/armor/* ./src/game/client/person/equipment/gun/* ./src/game/client/person/equipment/bullet/*
+javac -classpath ./lib/lwjgl.jar;./lib/slick-util.jar -sourcepath ./src -d ./bin -encoding UTF-8 ./src/engine/Loader.java ./src/engine/net/server/GameServer.java ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*
 pause
--- Compile.sh
+++ Compile.sh
@@ -1,3 +1,3 @@
 rm -rf ./bin;
 mkdir ./bin;
-javac -classpath ./lib/lwjgl.jar:./lib/slick-util.jar -sourcepath ./src -d ./bin ./src/engine/Loader.java ./src/engine/net/server/GameServer.java ./src/game/client/person/equipment/armor/* ./src/game/client/person/equipment/gun/* ./src/game/client/person/equipment/bullet/*;
+javac -classpath ./lib/lwjgl.jar:./lib/slick-util.jar -sourcepath ./src -d ./bin ./src/engine/Loader.java ./src/engine/net/server/GameServer.java ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*;
