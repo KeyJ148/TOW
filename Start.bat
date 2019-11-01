be499d64749ecfb943cb756f0bc7b8cc06e62132
Евгений Серов <kejik148@gmail.com>
2019-11-01 18:58:07 +0300

    Исправлены скрипты для компиляции в соответствие с изменением точки входа

--- Start.bat
+++ Start.bat
@@ -1,2 +1,2 @@
-java -classpath ./bin/;./lib/lwjgl.jar;./lib/slick-util.jar -Djava.library.path=./lib/native engine.Loader
+java -classpath ./bin/;./lib/lwjgl.jar;./lib/slick-util.jar -Djava.library.path=./lib/native game.Start
 pause
--- Start.sh
+++ Start.sh
@@ -1,1 +1,1 @@
-java -classpath ./bin/:./lib/lwjgl.jar:./lib/slick-util.jar -Djava.library.path=./lib/native engine.Loader;
+java -classpath ./bin/:./lib/lwjgl.jar:./lib/slick-util.jar -Djava.library.path=./lib/native game.Start;
