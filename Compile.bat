6741b4f8e7f79f2e610ceeaaf39949f7669baa95
Евгений Серов <kejik148@gmail.com>
2019-11-17 21:44:36 +0300

    Исправлен баг с файлом компиляции

--- Compile.bat
+++ Compile.bat
@@ -1,4 +1,4 @@
 rmdir /s/q bin
 mkdir bin
-javac -classpath ./lib/lwjgl.jar;./lib/slick-util.jar -sourcepath ./src -d ./bin -encoding UTF-8 ./src/game/Start.java ./src/engine/net/server/GameServer.java ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*
+javac -classpath ./lib/lwjgl.jar;./lib/slick-util.jar -sourcepath ./src -d ./bin -encoding UTF-8 ./src/game/GameStart.java ./src/game/ServerStart.java  ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*
 pause
