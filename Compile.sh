713af03600326f1132b44415e73822b5d48d2e36
Евгений Серов <kejik148@gmail.com>
2019-12-15 21:13:01 +0300

    Добавлено право на исполнение для файлов компиляции и запуска Исправлены ошибки в скрипте компиляции

--- Compile.sh
+++ Compile.sh
@@ -1,3 +1,3 @@
 rm -rf ./bin;
 mkdir ./bin;
-javac -classpath ./lib/lwjgl.jar:./lib/slick-util.jar -sourcepath ./src -d ./bin ./src/game/Start.java ./src/engine/net/server/GameServer.java ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*;
+javac -classpath ./lib/lwjgl.jar:./lib/slick-util.jar -sourcepath ./src -d ./bin ./src/game/GameStart.java ./src/game/ServerStart.java ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*;
