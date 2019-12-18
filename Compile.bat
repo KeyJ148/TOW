bd41319068988a19cecf241c02c0c552ba021ba9
Евгений Серов <kejik148@gmail.com>
2019-12-19 00:11:35 +0300

    Удалены старые скрипты запуска Описание работы сети (Network.txt) перенесено из корня в архив

--- Compile.bat
+++
@@ -1,4 +0,0 @@
-rmdir /s/q bin
-mkdir bin
-javac -classpath ./lib/lwjgl.jar;./lib/slick-util.jar -sourcepath ./src -d ./bin -encoding UTF-8 ./src/game/GameStart.java ./src/game/ServerStart.java  ./src/game/client/tanks/equipment/armor/* ./src/game/client/tanks/equipment/gun/* ./src/game/client/tanks/equipment/bullet/*
-pause
