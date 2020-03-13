d664386cf069cbc6f59d11f590e5313a73b75de7
Евгений Серов <kejik148@gmail.com>
2020-03-13 22:48:03 +0300

    Создан класс Sprite для объединения текстуры и маски Создан Loader и Storage для класса Sprite

---
+++ src/main/java/tow/engine/resources/sprites/Sprite.java
@@ -0,0 +1,23 @@
+package tow.engine.resources.sprites;
+
+import tow.engine.image.Mask;
+import tow.engine.resources.textures.Texture;
+
+public class Sprite {
+
+    private Texture texture;
+    private Mask mask;
+
+    public Sprite(Texture texture, Mask mask){
+        this.texture = texture;
+        this.mask = mask;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public Mask getMask() {
+        return mask;
+    }
+}
