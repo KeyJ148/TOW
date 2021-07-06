e907bbef22e23faa0c73adcb5a2fb13a4fba9355
KeyJ148 <kejik148@gmail.com>
2021-07-06 19:55:49 +0300

    Change menu background to texture

--- src/main/java/cc/abro/tow/client/menu/MenuLocation.java
+++ src/main/java/cc/abro/tow/client/menu/MenuLocation.java
@@ -5,23 +5,26 @@ import cc.abro.orchengine.gameobject.components.gui.EventableGuiPanelElement;
 import cc.abro.orchengine.image.Color;
 import cc.abro.orchengine.map.Background;
 import cc.abro.orchengine.map.Location;
+import cc.abro.orchengine.resources.sprites.SpriteStorage;
+import cc.abro.orchengine.resources.textures.TextureLoader;
 import cc.abro.orchengine.services.GuiElementService;
+import cc.abro.tow.client.ClientData;
 import cc.abro.tow.client.menu.panels.controllers.ClickChangePanelController;
 import cc.abro.tow.client.menu.panels.controllers.main.ClickExitController;
 import cc.abro.tow.client.menu.panels.events.ClickChangePanelGuiEvent;
 import cc.abro.tow.client.menu.panels.gui.MainMenuGuiPanel;
 
-import java.util.Set;
-
 public class MenuLocation extends Location {
+
+    private static final String BACKGROUND_SPRITE_NAME = "b_mass";
 
     public MenuLocation() {
         super(Global.engine.render.getWidth(), Global.engine.render.getHeight());
-        background = new Background(new Color(0, 150, 14), Color.GREEN);
+        background = new Background(Global.spriteStorage.getSprite(BACKGROUND_SPRITE_NAME).getTexture());
 
         MainMenuGuiPanel guiPanel = Global.guiPanelStorage.getPanel(MainMenuGuiPanel.class);
         EventableGuiPanelElement<MainMenuGuiPanel> guiElement = new EventableGuiPanelElement<>(guiPanel,
-                ClickChangePanelController.CONTROLLERS.get(MainMenuGuiPanel.class).get());
+                ClientData.panelControllersStorage.getControllers((MainMenuGuiPanel.class)));
         new GuiElementService().addGuiElementOnLocationCenter(guiElement, this);
     }
 }
