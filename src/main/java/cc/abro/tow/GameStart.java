848743c3e844a970aef289eaa23eb441ff3f6b0c
KeyJ148 <kejik148@gmail.com>
2021-08-15 01:35:55 +0300

    Refactoring processes of start and finish the engine

--- src/main/java/cc/abro/tow/GameStart.java
+++ src/main/java/cc/abro/tow/GameStart.java
@@ -1,6 +1,6 @@
 package cc.abro.tow;
 
-import cc.abro.orchengine.Loader;
+import cc.abro.orchengine.OrchEngine;
 import cc.abro.tow.client.Game;
 import cc.abro.tow.client.NetGameRead;
 import cc.abro.tow.server.NetServerRead;
@@ -9,6 +9,6 @@ import cc.abro.tow.server.Server;
 public class GameStart {
 
     public static void main(String[] args) {
-        Loader.start(Game.class, NetGameRead.class, Server.class, NetServerRead.class);
+        OrchEngine.start(Game.class, NetGameRead.class, Server.class, NetServerRead.class);
     }
 }
