44543bde3f760db6824d51dcb61785e09c9b5c27
KeyJ148 <kejik148@gmail.com>
2021-09-19 11:23:57 +0300

    Sorting in tab menu by wins

--- src/main/java/cc/abro/tow/client/map/BattleLocation.java
+++ src/main/java/cc/abro/tow/client/map/BattleLocation.java
@@ -1,28 +1,25 @@
 package cc.abro.tow.client.map;
 
 import cc.abro.orchengine.Manager;
-import cc.abro.orchengine.analysis.Analyzer;
 import cc.abro.orchengine.cycle.Render;
 import cc.abro.orchengine.gameobject.components.gui.GuiElement;
 import cc.abro.orchengine.map.Border;
 import cc.abro.orchengine.net.client.PingChecker;
-import cc.abro.orchengine.net.server.GameServer;
 import cc.abro.orchengine.services.GuiElementService;
 import cc.abro.tow.client.ClientData;
-import cc.abro.tow.client.DebugInfoGuiPanel;
 import cc.abro.tow.client.GameLocation;
 import cc.abro.tow.client.GameTabGuiPanel;
 import cc.abro.tow.client.map.specification.MapObjectSpecification;
 import cc.abro.tow.client.map.specification.MapSpecification;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static cc.abro.tow.client.menu.InterfaceStyles.*;
+import static cc.abro.tow.client.menu.InterfaceStyles.TAB_LINE_SIZE_Y;
+import static cc.abro.tow.client.menu.InterfaceStyles.TAB_SIZE_X;
 
 public class BattleLocation extends GameLocation {
 
@@ -54,6 +51,7 @@ public class BattleLocation extends GameLocation {
                             .filter(tank -> tank.getName() != null)
                             .map(tank -> new GameTabGuiPanel.TabDataLine(!tank.alive,
                                     tank.getName(), tank.color, tank.kill, tank.death, tank.win, ping))
+                            .sorted(Comparator.comparingInt(t -> t.wins))
                             .collect(Collectors.toList());
                     getComponent().fillInTable(data);
                 } else {
