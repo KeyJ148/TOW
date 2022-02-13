4c8d9115fabf0fc0d3a933d4e1017dd733fc92a6
KeyJ148 <kejik148@gmail.com>
2022-02-13 12:55:16 +0300

    Fix after merge

--- src/main/java/cc/abro/tow/client/menu/panels/settings/SettingsMenuGuiPanel.java
+++ src/main/java/cc/abro/tow/client/menu/panels/settings/SettingsMenuGuiPanel.java
@@ -1,34 +1,13 @@
 package cc.abro.tow.client.menu.panels.settings;
 
-import cc.abro.orchengine.Manager;
-import cc.abro.orchengine.gameobject.components.Position;
-import cc.abro.orchengine.gui.MouseReleaseBlockingListeners;
 import cc.abro.orchengine.gui.TabPanel;
-import cc.abro.orchengine.image.Color;
-import cc.abro.orchengine.resources.sprites.SpriteStorage;
-import cc.abro.orchengine.resources.textures.Texture;
-import cc.abro.orchengine.resources.textures.TextureLoader;
-import cc.abro.orchengine.services.BlockingGuiService;
-import cc.abro.orchengine.services.GuiService;
-import cc.abro.tow.client.SettingsStorage;
-import cc.abro.tow.client.menu.panels.MainMenuGuiPanel;
 import cc.abro.tow.client.menu.panels.MenuGuiPanel;
-import cc.abro.tow.client.services.SettingsService;
-import org.joml.Vector2f;
-import org.liquidengine.legui.component.*;
-import org.liquidengine.legui.event.MouseClickEvent;
-import org.liquidengine.legui.image.FBOImage;
-import org.liquidengine.legui.listener.MouseClickEventListener;
-import org.liquidengine.legui.style.Background;
+import org.liquidengine.legui.component.Component;
+import org.liquidengine.legui.component.Panel;
 
-import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 import static cc.abro.tow.client.menu.InterfaceStyles.*;
-import static cc.abro.tow.client.menu.MenuGuiComponents.*;
-import static cc.abro.tow.client.menu.panels.FirstEntryGuiPanel.Error.CANT_SAVE_SETTINGS;
-import static cc.abro.tow.client.menu.panels.FirstEntryGuiPanel.Error.NICKNAME_IS_EMPTY;
-import static cc.abro.tow.client.menu.MenuGuiComponents.*;
 
 public class SettingsMenuGuiPanel extends MenuGuiPanel{
     protected final static int PANEL_COLOR_WIDTH = 45;
