48007d1ba6584e8e1685a676681967e40bf6b89e
KeyJ148 <kejik148@gmail.com>
2022-04-16 17:29:29 +0300

    #69 Refactor Chunk, DepthVector, MapControl. Fix bug with duplicate equipments again.

--- src/main/java/cc/abro/tow/client/tanks/enemy/EnemyArmor.java
+++ src/main/java/cc/abro/tow/client/tanks/enemy/EnemyArmor.java
@@ -13,8 +13,8 @@ public class EnemyArmor extends GameObject {
 
     public Enemy enemy;
 
-    public EnemyArmor(int x, int y, double direction, int depth, Animation animation, Enemy enemy) {
-        super(Arrays.asList(new Position(x, y, depth, (int) direction), new AnimationRender(animation.getTextures())));
+    public EnemyArmor(int x, int y, double direction, int z, Animation animation, Enemy enemy) {
+        super(Arrays.asList(new Position(x, y, z, (int) direction), new AnimationRender(animation.getTextures())));
         this.enemy = enemy;
 
         setComponent(new Movement());
