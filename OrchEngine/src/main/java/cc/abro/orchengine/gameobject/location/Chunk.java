efd864917efe2067d4c6be81d9f6ef5cea412a61
KeyJ148 <kejik148@gmail.com>
2023-02-05 12:39:08 +0400

    #69 Refactoring GameObject and Location systems

--- OrchEngine/src/main/java/cc/abro/orchengine/gameobject/location/Chunk.java
+++ OrchEngine/src/main/java/cc/abro/orchengine/gameobject/location/Chunk.java
@@ -1,50 +1,37 @@
 package cc.abro.orchengine.gameobject.location;
 
-import cc.abro.orchengine.gameobject.GameObject;
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 
-public class Chunk {
+public class Chunk<T> {
 
     public static final int DEFAULT_SIZE = 100;
 
-    private final Set<GameObject> objects = new HashSet<>();
+    private final Set<T> objects = new HashSet<>();
 
-    public void add(GameObject gameObject) {
-        objects.add(gameObject);
+    public void add(T object) {
+        objects.add(object);
     }
 
-    public void remove(GameObject gameObject) {
-        objects.remove(gameObject);
+    public void remove(T object) {
+        objects.remove(object);
     }
 
-    public void update(long delta) {
+    public void runForAll(Consumer<T> runnable) {
         //Делаем копию сета, иначе получаем ConcurrentModificationException,
         //т.к. во время апдейта можно создать новый объект и этот объект будет помещен в сет
-        new ArrayList<>(objects).forEach(gameObject -> gameObject.update(delta)); //TODO в подобных местах использовать Collections.unmodifiableSet() ?
-    }
-
-    public void render() {
-        for (GameObject gameObject : objects) {
-            gameObject.draw();
-        }
-    }
-
-    public void destroy() {
-        for (GameObject gameObject : objects) {
-            gameObject.destroy();
-        }
+        new ArrayList<>(objects).forEach(runnable);  //TODO в подобных местах использовать Collections.unmodifiableSet() ?
     }
 
     @Deprecated
-    public List<GameObject> getObjects() {
-        return new ArrayList<>(objects);
+    public Set<T> getObjects() {
+        return Collections.unmodifiableSet(objects);
     }
 
-    public int size() {
+    public int objectsCount() {
         return objects.size();
     }
 }
