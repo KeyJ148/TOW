0ad873ec404af7a9533a72ca13a6d7424e8e06a2
Bronuh <bronyx@yandex.ru>
2023-03-28 18:07:14 +0300

    Added Kotlin support

---
+++ src/main/kotlin/cc/abro/tow/KotlinTest.kt
@@ -0,0 +1,13 @@
+package cc.abro.tow
+
+class KotlinTest {
+    companion object {
+        /**
+         * It'll return some kotlin or something
+         */
+        @JvmStatic
+        public fun testKotlin() : String{
+            return "Some Kotlin"
+        }
+    }
+}
