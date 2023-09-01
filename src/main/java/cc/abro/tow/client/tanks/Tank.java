a445f0015e33c25921da151daffc9375bb8c8ca3
KeyJ148 <kejik148@gmail.com>
2023-09-01 11:41:39 +0300

    #79 Create new tank GameObject

--- src/main/java/cc/abro/tow/client/tanks/Tank.java
+++ src/main/java/cc/abro/tow/client/tanks/Tank.java
@@ -3,120 +3,153 @@ package cc.abro.tow.client.tanks;
 import cc.abro.orchengine.context.Context;
 import cc.abro.orchengine.gameobject.GameObject;
 import cc.abro.orchengine.gameobject.Location;
-import cc.abro.orchengine.gameobject.LocationManager;
 import cc.abro.orchengine.gameobject.components.Movement;
 import cc.abro.orchengine.gameobject.components.PositionableComponent;
-import cc.abro.orchengine.gameobject.components.particles.Particles;
-import cc.abro.orchengine.gameobject.components.render.AnimationRender;
-import cc.abro.orchengine.gameobject.components.render.Rendering;
+import cc.abro.orchengine.gameobject.components.render.SpriteRender;
 import cc.abro.orchengine.image.Color;
-import cc.abro.orchengine.util.GameObjectFactory;
-import cc.abro.tow.client.ClientData;
-import cc.abro.tow.client.GameSetting;
+import cc.abro.orchengine.resources.audios.AudioService;
 import cc.abro.tow.client.particles.Explosion;
+import cc.abro.tow.client.settings.GameSettingsService;
+import cc.abro.tow.client.tanks.components.AnimationOnMovementComponent;
 import cc.abro.tow.client.tanks.components.TankNicknameComponent;
-import cc.abro.tow.client.tanks.enemy.Enemy;
+import cc.abro.tow.client.tanks.components.TankStatsComponent;
+import cc.abro.tow.client.tanks.equipment.armor.ArmorComponent;
+import cc.abro.tow.client.tanks.equipment.gun.GunComponent;
 import lombok.Getter;
-
-import java.util.Map;
 
 public abstract class Tank extends GameObject {
 
     private static final Color EXPLODED_TANK_COLOR = new Color(110, 15, 0);
 
-    private final LocationManager locationManager;
+    private final AudioService audioService;
+    private final GameSettingsService gameSettingsService;
 
-    private final PositionableComponent camera;
+    private final PositionableComponent cameraComponent;
     private final TankNicknameComponent tankNicknameComponent;
-
-    public GameObject armor;
-    public GameObject gun;
+    @Getter
+    private final Movement movementComponent;
+    @Getter
+    private final TankStatsComponent tankStatsComponent;
+    @Getter
+    private AnimationOnMovementComponent armorAnimationComponent;
+    @Getter
+    private SpriteRender gunSpriteComponent;
+    @Getter
+    private ArmorComponent armorComponent;
+    @Getter
+    private GunComponent gunComponent;
 
     @Getter
-    protected Color color = Color.WHITE;
-    public boolean alive = true;
+    private boolean alive = true;
+    @Getter
+    private Color color = Color.WHITE;
 
-    public int kill = 0;
-    public int death = 0;
-    public int win = 0;
-
-    public Tank(Location location) {
+    public Tank(Location location, double x, double y, double direction,
+                ArmorComponent armorComponent, GunComponent gunComponent) {
         super(location);
-        locationManager = getLocationManager();
+        audioService = getAudioService();
+        gameSettingsService = Context.getService(GameSettingsService.class);
 
-        camera = new PositionableComponent();
-        addComponent(camera);
+        setX(x);
+        setY(y);
+        setDirection(direction);
+
+        cameraComponent = new PositionableComponent();
+        addComponent(cameraComponent);
 
         tankNicknameComponent = new TankNicknameComponent();
         addComponent(tankNicknameComponent);
+
+        movementComponent = new Movement();
+        addComponent(movementComponent);
+
+        this.armorComponent = armorComponent;
+        addComponent(armorComponent);
+
+        armorAnimationComponent = new AnimationOnMovementComponent(armorComponent.getAnimation().textures(), 1000,
+                armorComponent.getAnimationSpeedCoefficient()); //TODO вынести глубины в константы
+        addComponent(armorAnimationComponent);
+
+        this.gunComponent = gunComponent;
+        addComponent(gunComponent);
+
+        gunSpriteComponent = new SpriteRender(gunComponent.getSprite().texture(), 2000); //TODO вынести глубины в константы
+        addComponent(gunSpriteComponent);
+
+        tankStatsComponent = new TankStatsComponent();
+        addComponent(tankNicknameComponent);
+
+        //TODO other components ...
+
+        changeArmor(armorComponent);
     }
 
     public void exploded() {
         alive = false;
-        death++;
 
         tankNicknameComponent.destroy();
+        movementComponent.destroy();
+        armorAnimationComponent.setFrameSpeed(0);
 
-        if (armor != null) {
-            armor.getComponent(Movement.class).speed = 0;
-            ((AnimationRender) armor.getComponent(Rendering.class)).setFrameSpeed(0);
+        setColor(EXPLODED_TANK_COLOR);
 
-            setColor(EXPLODED_TANK_COLOR);
+        Explosion explosionParticlesComponent = new Explosion(100);
+        explosionParticlesComponent.activate();
+        addComponent(explosionParticlesComponent);
 
-            GameObject explosion = GameObjectFactory.create(getLocation(), armor.getX(), armor.getY());
-            Explosion explosionParticles = new Explosion(100);
-            explosion.addComponent(explosionParticles);
-            explosionParticles.activate();
-            explosion.getComponent(Particles.class).destroyObject = true;
-        }
-
-        //Если в данный момент камера установлена на этот объект
-        if (locationManager.getActiveLocation().getCamera().hasFollowObject() &&
-                locationManager.getActiveLocation().getCamera().getFollowObject().orElse(null) == camera) {
-            //Выбираем живого врага с инициализированной камерой, переносим камеру туда
-            for (Map.Entry<Integer, Enemy> entry : Context.getService(ClientData.class).enemy.entrySet()) {
-                if (entry.getValue().alive) {
-                    entry.getValue().setLocationCameraToThisObject();
-                    break;
-                }
-            }
-        }
-
-        getAudioService().playSoundEffect(getAudioStorage().getAudio("explosion"), (int) getX(), (int) getY(), GameSetting.SOUND_RANGE);
+        audioService.playSoundEffect(getAudioStorage().getAudio("explosion"), (int) getX(), (int) getY(),
+                gameSettingsService.getGameSettings().getSoundRange());
     }
 
-    public void replaceArmor(GameObject newArmor) {
-        //Устанавливаем новой броне параметры как у текущий брони танка
-        newArmor.getComponent(Movement.class).setDirection(armor.getComponent(Movement.class).getDirection());
+    public void changeArmor(ArmorComponent newArmorComponent) {
+        double lastMaxHp = tankStatsComponent.getStats().hpMax;
+        tankStatsComponent.removeEffect(armorComponent.getEffect());
 
-        armor.destroy();
-        armor = newArmor;
+        armorComponent.destroy();
+        removeComponent(armorComponent);
+        armorAnimationComponent.destroy();
+        removeComponent(armorAnimationComponent);
 
-        setColorArmor(color);
+        armorComponent = newArmorComponent;
+        addComponent(armorComponent);
+        armorAnimationComponent = new AnimationOnMovementComponent(newArmorComponent.getAnimation().textures(),
+                1000, newArmorComponent.getAnimationSpeedCoefficient()); //TODO вынести глубины в константы
+        armorAnimationComponent.setColor(color);
+        addComponent(armorAnimationComponent);
+
+        tankStatsComponent.addEffect(armorComponent.getEffect());
+
+        //Устанавливаем эквивалентное здоровье в процентах
+        double lastCurrentHp = tankStatsComponent.getCurrentHp();
+        double newMaxHp = tankStatsComponent.getStats().hpMax;
+        double newCurrentHp = lastMaxHp != 0 ?
+                lastCurrentHp / lastMaxHp * newMaxHp :
+                newMaxHp;
+        tankStatsComponent.setCurrentHp(newCurrentHp);
     }
 
-    public void replaceGun(GameObject newGun) {
-        newGun.setDirection(gun.getDirection());
+    public void changeGun(GunComponent newGunComponent) {
+        tankStatsComponent.removeEffect(gunComponent.getEffect());
 
-        gun.destroy();
-        gun = newGun;
-        setColorGun(color);
+        gunComponent.destroy();
+        removeComponent(gunComponent);
+        gunSpriteComponent.destroy();
+        removeComponent(gunSpriteComponent);
+
+        gunComponent = newGunComponent;
+        addComponent(armorComponent);
+        gunSpriteComponent = new SpriteRender(newGunComponent.getSprite().texture(), 2000); //TODO вынести глубины в константы
+        gunSpriteComponent.setColor(color);
+        addComponent(gunSpriteComponent);
+
+        tankStatsComponent.addEffect(gunComponent.getEffect());
+        gunSpriteComponent.setColor(color);
     }
 
-    public void setColor(Color c) {
-        color = c;
-        setColorArmor(c);
-        setColorGun(c);
-    }
-
-    public void setColorArmor(Color c) {
-        if (armor == null || !armor.hasComponent(Rendering.class)) return;
-        armor.getComponent(Rendering.class).setColor(c);
-    }
-
-    public void setColorGun(Color c) {
-        if (gun == null || !gun.hasComponent(Rendering.class)) return;
-        gun.getComponent(Rendering.class).setColor(c);
+    public void setColor(Color color) {
+        this.color = color;
+        armorAnimationComponent.setColor(color);
+        gunSpriteComponent.setColor(color);
     }
 
     public void setNickname(String nickname) {
@@ -128,11 +161,11 @@ public abstract class Tank extends GameObject {
     }
 
     public void setLocationCameraToThisObject() {
-        getLocation().getCamera().setFollowObject(camera);
+        getLocation().getCamera().setFollowObject(cameraComponent);
     }
 
     public boolean locationCameraIsThisObject() {
         return getLocation().getCamera().getFollowObject()
-                .filter(locationCamera -> locationCamera == camera).isPresent();
+                .filter(locationCamera -> locationCamera == cameraComponent).isPresent();
     }
 }
