b609ea6f429499d8468499828a37f4c2d579eb90
KeyJ148 <kejik148@gmail.com>
2023-09-12 19:40:40 +0300

    #79 Update BattleStatisticService

--- src/main/java/cc/abro/tow/client/services/BattleStatisticService.java
+++ src/main/java/cc/abro/tow/client/services/BattleStatisticService.java
@@ -12,10 +12,30 @@ public class BattleStatisticService {
     private final Map<Integer, BattleStatistic> battleStatisticByEnemyId = new HashMap<>();
     private final BattleStatistic playerBattleStatistic = new BattleStatistic();
 
+    public BattleStatistic getPlayerStatistic() {
+        return playerBattleStatistic;
+    }
+
+    public BattleStatistic getEnemyStatistic(int id) {
+        return battleStatisticByEnemyId.computeIfAbsent(id, (key) -> new BattleStatistic());
+    }
+
     @Data
     public static class BattleStatistic {
         private int kill;
         private int death;
         private int win;
+
+        public void incrementKill() {
+            setKill(getKill() + 1);
+        }
+
+        public void incrementDeath() {
+            setDeath(getDeath() + 1);
+        }
+
+        public void incrementWin() {
+            setWin(getWin() + 1);
+        }
     }
 }
