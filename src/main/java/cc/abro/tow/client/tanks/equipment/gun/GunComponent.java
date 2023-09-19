5a858c88e28530259f82bb687a7d56ca1c8e2601
KeyJ148 <kejik148@gmail.com>
2023-09-19 22:11:25 +0300

    #79 Release functions gun component

--- src/main/java/cc/abro/tow/client/tanks/equipment/gun/GunComponent.java
+++ src/main/java/cc/abro/tow/client/tanks/equipment/gun/GunComponent.java
@@ -1,8 +1,9 @@
 package cc.abro.tow.client.tanks.equipment.gun;
 
 import cc.abro.orchengine.gameobject.Component;
-import cc.abro.orchengine.gameobject.GameObject;
+import cc.abro.orchengine.gameobject.components.interfaces.Updatable;
 import cc.abro.orchengine.resources.sprites.Sprite;
+import cc.abro.tow.client.tanks.Tank;
 import cc.abro.tow.client.tanks.stats.Effect;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 @RequiredArgsConstructor
-public abstract class GunComponent extends Component<GameObject> {
+public abstract class GunComponent extends Component<Tank> implements Updatable {
     @Getter
     private final String name; //Имя файла и техническое имя брони
     @Getter
@@ -19,50 +20,52 @@ public abstract class GunComponent extends Component<GameObject> {
     private final Effect effect;
     @Getter
     private final Sprite sprite; //TODO переделать gun и armor на использование Render компонента, без привязки спрайт/текстура
-
-    public record TrunkInfo(int bulletStartX, int bulletStartY, double bulletStartDir) {}
     @Getter
     private final List<TrunkInfo> trunksInfo;
-
-
-    //TODO разобрать
-    /*
-    public long nanoSecFromAttack = 0;//Кол-во времени до конца перезарядки в наносекундах
+    @Getter
+    private long nanoSecToAttack = 0;//Кол-во времени до конца перезарядки в наносекундах
 
     @Override
     public void update(long delta) {
-        //Если мы мертвы, то ничего не делать
-        if (!player.alive) return;
-
         //Уменьшаем время до выстрела
-        nanoSecFromAttack -= delta;
+        nanoSecToAttack -= delta;
     }
 
-    public void attack() {
-        nanoSecFromAttack = (long) ((double) 1 / player.stats.attackSpeed * Math.pow(10, 9)); //Устанавливаем время перезарядки
+    public void tryAttack() {
+        if (nanoSecToAttack > 0 || getGameObject().getTankStatsComponent().getStats().attackSpeed <= 0){
+            return;
+        }
+
+        double attackSpeed = getGameObject().getTankStatsComponent().getStats().attackSpeed;
+        nanoSecToAttack = (long) (Math.pow(10, 9) / attackSpeed); //Устанавливаем время перезарядки
 
         //По очереди стреляем из всех стволов
-        for (int i = 0; i < countTrunk; i++) {
-            attackFromTrunk(trunksOffset[i].x, trunksOffset[i].y, directionTrunk[i]);
+        for (TrunkInfo trunk : trunksInfo) {
+            attackFromTrunk(trunk.bulletStartX, trunk.bulletStartY, trunk.bulletStartDir);
         }
     }
 
-    private void attackFromTrunk(int trunkX, int trunkY, double direction) {
-        double trunkXdx = trunkX * Math.cos(Math.toRadians(getDirection()) - Math.PI / 2);//первый отступ "вперед"
-        double trunkXdy = trunkX * Math.sin(Math.toRadians(getDirection()) - Math.PI / 2);//в отличие от маски мы отнимаем от каждого по PI/2
-        double trunkYdx = trunkY * Math.cos(Math.toRadians(getDirection()) - Math.PI);//потому что изначально у теустуры измененное направление
-        double trunkYdy = trunkY * Math.sin(Math.toRadians(getDirection()) - Math.PI);//второй отступ "вбок"
+    private void attackFromTrunk(int trunkX, int trunkY, double bulletStartDir) {
+        double gunDirection = Math.toRadians(getGameObject().getGunSpriteComponent().getDirection());
+        double trunkXdx = trunkX * Math.cos(gunDirection - Math.PI / 2); //первый отступ "вперед"
+        double trunkXdy = trunkX * Math.sin(gunDirection - Math.PI / 2); //в отличие от маски мы отнимаем от каждого по PI/2
+        double trunkYdx = trunkY * Math.cos(gunDirection - Math.PI); //потому что изначально у теустуры измененное направление
+        double trunkYdy = trunkY * Math.sin(gunDirection - Math.PI); //второй отступ "вбок"
 
+        //TODO создание пули
+        /*
         Bullet newBullet = player.bullet.create();
         newBullet.init(
                 player,
                 getX() + trunkXdx + trunkYdx,
                 getY() - trunkXdy - trunkYdy,
-                getDirection() + direction,
+                getDirection() + bulletStartDir,
                 player.stats.damage,
                 player.stats.range,
                 player.bullet.name
         );
-    }*/
+        */
+    }
 
+    public record TrunkInfo(int bulletStartX, int bulletStartY, double bulletStartDir) {}
 }
