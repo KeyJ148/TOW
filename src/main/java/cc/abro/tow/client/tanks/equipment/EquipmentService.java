79b0d606139a17517520b195bd1128ef297eb38f
KeyJ148 <kejik148@gmail.com>
2023-09-20 22:12:02 +0300

    #79 Setup EquipmentService

--- src/main/java/cc/abro/tow/client/tanks/equipment/EquipmentService.java
+++ src/main/java/cc/abro/tow/client/tanks/equipment/EquipmentService.java
@@ -1,8 +1,10 @@
 package cc.abro.tow.client.tanks.equipment;
 
 import cc.abro.orchengine.context.GameService;
+import cc.abro.tow.client.ClientData;
 import cc.abro.tow.client.map.objects.Box;
 import cc.abro.tow.client.tanks.Tank;
+import cc.abro.tow.client.tanks.components.PlayerTankEquipmentControllerComponent;
 import cc.abro.tow.client.tanks.equipment.armor.ArmorComponent;
 import cc.abro.tow.client.tanks.equipment.armor.ArmorCreationService;
 import cc.abro.tow.client.tanks.equipment.gun.GunComponent;
@@ -18,32 +20,61 @@ public class EquipmentService {
 
     private final ArmorCreationService armorCreationService;
     private final GunCreationService gunCreationService;
+    private final ClientData clientData;
+
+    public void changeEquipment(Tank tank, Box box) {
+        PlayerTankEquipmentControllerComponent equipmentController =
+                clientData.player.getPlayerTankEquipmentControllerComponent();
+
+        switch (box.getType()) {
+            case ARMOR -> {
+                if (!equipmentController.isPlayerCanTakeArmor()) return;
+                setNewArmor(tank);
+            }
+            case GUN -> {
+                if (!equipmentController.isPlayerCanTakeGun()) return;
+                setNewGun(tank);
+            }
+            case BULLET -> {
+                if (!equipmentController.isPlayerCanTakeBullet()) return;
+                setNewBullet(tank);
+            }
+            case HEALTH -> {
+                if (!equipmentController.isPlayerCanTakeHeal()) return;
+                clientData.player.changeHp(clientData.player.getTankStatsComponent().getStats().hpMax * 0.4);
+            }
+            case HEALTH_FULL -> {
+                if (!equipmentController.isPlayerCanTakeHeal()) return;
+                clientData.player.changeHp(clientData.player.getTankStatsComponent().getStats().hpMax);
+            }
+        }
+    }
+
+    public void setNewArmor(Tank tank) {
+        //TODO
+    }
+
+    public void setNewGun(Tank tank) {
+        //TODO
+    }
+
+    public void setNewBullet(Tank tank) {
+        //TODO
+    }
 
     public ArmorComponent createDefaultArmor() {
         return armorCreationService.createArmor(DEFAULT_ARMOR_NAME);
     }
 
-    public GunComponent createGunArmor() {
+    public GunComponent createDefaultGun() {
         return gunCreationService.createGun(DEFAULT_GUN_NAME);
     }
 
-    public void changeEquipment(Tank tank, Box box) {
-        switch (box.getTypeBox()) {
-            case 0 -> {
-                if (player.takeArmor) EquipManager.newArmor(player);
-            }
-            case 1 -> {
-                if (player.takeGun) EquipManager.newGun(player);
-            }
-            case 2 -> {
-                if (player.takeBullet) EquipManager.newBullet(player);
-            }
-            case 3 -> {
-                if (player.takeHealth) player.hp = (long) (player.hp + player.stats.hpMax * 0.4);
-            }
-            case 4 -> {
-                if (player.takeHealth) player.hp = (long) player.stats.hpMax;
-            }
-        }
+    public ArmorComponent createArmor(String armorName) {
+        return armorCreationService.createArmor(armorName);
+    }
+
+    public GunComponent createGun(String gunName) {
+        return gunCreationService.createGun(gunName);
     }
 }
