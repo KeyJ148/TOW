3300d60e7ef8d337dd0822cd577046d751f311a1
KeyJ148 <kejik148@gmail.com>
2023-10-02 13:20:45 +0300

    #79 Add all types of gun and armor to EquipmentCreatorsLoader

--- src/main/java/cc/abro/tow/client/tanks/equipment/EquipmentCreatorsLoader.java
+++ src/main/java/cc/abro/tow/client/tanks/equipment/EquipmentCreatorsLoader.java
@@ -3,6 +3,12 @@ package cc.abro.tow.client.tanks.equipment;
 import cc.abro.orchengine.context.GameService;
 import cc.abro.tow.client.tanks.equipment.armor.ArmorCreatorsStorage;
 import cc.abro.tow.client.tanks.equipment.armor.defaults.DefaultArmorCreator;
+import cc.abro.tow.client.tanks.equipment.armor.fury.FuryArmorCreator;
+import cc.abro.tow.client.tanks.equipment.armor.vampire.VampireArmorCreator;
+import cc.abro.tow.client.tanks.equipment.gun.GunCreatorsStorage;
+import cc.abro.tow.client.tanks.equipment.gun.defaults.DefaultGunCreator;
+import cc.abro.tow.client.tanks.equipment.gun.fury.FuryGunCreator;
+import cc.abro.tow.client.tanks.equipment.gun.vampire.VampireGunCreator;
 import lombok.RequiredArgsConstructor;
 
 @GameService
@@ -10,8 +16,15 @@ import lombok.RequiredArgsConstructor;
 public class EquipmentCreatorsLoader {
 
     private final ArmorCreatorsStorage armorCreatorsStorage;
+    private final GunCreatorsStorage gunCreatorsStorage;
 
     public void load() { //TODO просто сканить аннотации и добавлять автоматически по аннотациям. Отдельные классы для брони/оружия/патрон. Общий сервис для скана по аннотациям.
         armorCreatorsStorage.addArmorCreator(new DefaultArmorCreator<>());
+        armorCreatorsStorage.addArmorCreator(new FuryArmorCreator<>());
+        armorCreatorsStorage.addArmorCreator(new VampireArmorCreator<>());
+
+        gunCreatorsStorage.addGunCreator(new DefaultGunCreator<>());
+        gunCreatorsStorage.addGunCreator(new FuryGunCreator<>());
+        gunCreatorsStorage.addGunCreator(new VampireGunCreator<>());
     }
 }
