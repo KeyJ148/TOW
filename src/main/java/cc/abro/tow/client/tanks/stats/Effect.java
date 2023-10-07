b0d982922352d11a5625dcbf6cf77c73a36138d4
KeyJ148 <kejik148@gmail.com>
2023-10-07 15:09:24 +0300

    #79 Refactoring stats class

--- src/main/java/cc/abro/tow/client/tanks/stats/Effect.java
+++ src/main/java/cc/abro/tow/client/tanks/stats/Effect.java
@@ -1,39 +1,9 @@
 package cc.abro.tow.client.tanks.stats;
 
+import lombok.Data;
+
+@Data
 public class Effect {
-
-    public Stats addition = new Stats();//Увеличение характеристик (+)
-    public Stats multi = new Stats(1);//Увеличение характеристик (*)
-
-    public void calcAddStats(Stats stats) {
-        stats.hpMax += addition.hpMax;
-        stats.hpRegen += addition.hpRegen;
-        stats.speedUp += addition.speedUp;
-        stats.speedDown += addition.speedDown;
-        stats.stability += addition.stability;
-
-        stats.speedRotateGun += addition.speedRotateGun;
-        stats.speedRotateTank += addition.speedRotateTank;
-
-        stats.attackSpeed += addition.attackSpeed;
-        stats.range += addition.range;
-        stats.damage += addition.damage;
-    }
-
-    public void calcMultiStats(Stats stats) { //TODO в начале суммировать множители, а потом умножать на них? Т.к. сейчас +30% и -30% = 1*1.3*0.7 = 0.91
-        stats.hpMax *= multi.hpMax;
-        stats.hpRegen *= multi.hpRegen;
-        stats.speedUp *= multi.speedUp;
-        stats.speedDown *= multi.speedDown;
-        stats.stability *= multi.stability;
-
-        stats.speedRotateGun *= multi.speedRotateGun;
-        stats.speedRotateTank *= multi.speedRotateTank;
-
-        stats.attackSpeed *= multi.attackSpeed;
-        stats.range *= multi.range;
-        stats.damage *= multi.damage;
-    }
-
-    //TODO equals, hashcode @Data и конструктор, и для stats
+    private Stats addition = Stats.builder().build(); //Увеличение характеристик (+)
+    private Stats multi = Stats.builder().setAllValue(1).build();//Увеличение характеристик (*)
 }
