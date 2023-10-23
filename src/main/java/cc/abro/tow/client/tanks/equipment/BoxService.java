687193e37a5e310528e37e58e913e4be07662b01
KeyJ148 <kejik148@gmail.com>
2023-10-24 00:07:30 +0300

    #190 Refactoring BoxService

--- src/main/java/cc/abro/tow/client/tanks/equipment/BoxService.java
+++ src/main/java/cc/abro/tow/client/tanks/equipment/BoxService.java
@@ -16,33 +16,53 @@ public class BoxService {
     private final ClientData clientData;
     private final EquipmentService equipmentService;
 
-    public void takeBox(Tank tank, Box box) {
+    public void takeBox(Tank tank, Box.Type boxType) {
         PlayerTankEquipmentControllerComponent equipmentController =
                 clientData.player.getPlayerTankEquipmentControllerComponent();
 
-        switch (box.getType()) {
+        switch (boxType) {
             case ARMOR -> {
                 if (!equipmentController.isPlayerCanTakeArmor()) return;
-                ArmorComponent armorComponent = equipmentService.createNewArmor(tank);
-                tank.changeArmor(armorComponent);
+                takeArmorBox(tank);
             }
             case GUN -> {
                 if (!equipmentController.isPlayerCanTakeGun()) return;
-                GunComponent gunComponent = equipmentService.createNewGun(tank);
-                tank.changeGun(gunComponent);
+                takeGunBox(tank);
             }
             case BULLET -> {
                 if (!equipmentController.isPlayerCanTakeBullet()) return;
-                equipmentService.createNewBullet(tank);
+                takeBulletBox(tank);
             }
             case HEALTH -> {
                 if (!equipmentController.isPlayerCanTakeHeal()) return;
-                clientData.player.changeHp(clientData.player.getTankStatsComponent().getStats().getHpMax() * 0.4);
+                takeHealthBox(tank);
             }
             case HEALTH_FULL -> {
                 if (!equipmentController.isPlayerCanTakeHeal()) return;
-                clientData.player.changeHp(clientData.player.getTankStatsComponent().getStats().getHpMax());
+                takeHealthFullBox(tank);
             }
         }
+    }
+
+    public void takeArmorBox(Tank tank) {
+        ArmorComponent armorComponent = equipmentService.createNewArmor(tank);
+        tank.changeArmor(armorComponent);
+    }
+
+    public void takeGunBox(Tank tank) {
+        GunComponent gunComponent = equipmentService.createNewGun(tank);
+        tank.changeGun(gunComponent);
+    }
+
+    public void takeBulletBox(Tank tank) {
+        equipmentService.createNewBullet(tank);
+    }
+
+    public void takeHealthBox(Tank tank) {
+        clientData.player.changeHp(clientData.player.getTankStatsComponent().getStats().getHpMax() * 0.4);
+    }
+
+    public void takeHealthFullBox(Tank tank) {
+        clientData.player.changeHp(clientData.player.getTankStatsComponent().getStats().getHpMax());
     }
 }
