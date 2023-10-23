ca612bcb0c97fe3a950a45cdccfc2295e8658c44
KeyJ148 <kejik148@gmail.com>
2023-10-24 00:07:12 +0300

    #79 Create service for process singleplayer buttons

---
+++ src/main/java/cc/abro/tow/client/services/SingleplayerControllerService.java
@@ -0,0 +1,37 @@
+package cc.abro.tow.client.services;
+
+import cc.abro.orchengine.context.GameService;
+import cc.abro.orchengine.events.input.KeyReleaseEvent;
+import cc.abro.tow.client.ClientData;
+import cc.abro.tow.client.tanks.equipment.BoxService;
+import com.google.common.eventbus.Subscribe;
+import lombok.RequiredArgsConstructor;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+@GameService
+@RequiredArgsConstructor
+public class SingleplayerControllerService {
+
+    private final ClientData clientData;
+    private final BoxService boxService;
+
+    @Subscribe
+    public void onKeyReleaseEvent(KeyReleaseEvent keyReleaseEvent) {
+        if (!isSingleplayerGame()) {
+            return;
+        }
+
+        switch (keyReleaseEvent.getKey()) {
+            case GLFW_KEY_T -> boxService.takeArmorBox(clientData.player);
+            case GLFW_KEY_G -> boxService.takeGunBox(clientData.player);
+            case GLFW_KEY_B -> boxService.takeBulletBox(clientData.player);
+            case GLFW_KEY_H -> boxService.takeHealthBox(clientData.player);
+            case GLFW_KEY_F -> boxService.takeHealthFullBox(clientData.player);
+        }
+    }
+
+    private boolean isSingleplayerGame() {
+        return clientData.peopleMax == 1;
+    }
+}
