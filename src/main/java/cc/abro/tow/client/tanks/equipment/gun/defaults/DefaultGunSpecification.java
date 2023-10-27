1859e861345593074f7be3c932f71a9680c4aa51
KeyJ148 <kejik148@gmail.com>
2023-10-28 01:08:54 +0300

    #191 Add bullet mapping to default gun specification and component

--- src/main/java/cc/abro/tow/client/tanks/equipment/gun/defaults/DefaultGunSpecification.java
+++ src/main/java/cc/abro/tow/client/tanks/equipment/gun/defaults/DefaultGunSpecification.java
@@ -6,20 +6,25 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class DefaultGunSpecification implements GunSpecification {
     public record TrunkInfo(int bulletStartX, int bulletStartY, double bulletStartDir) {}
+    public record BulletInfo(String behavior, String spriteName) {}
 
     private final double attackSpeed;
+    private final double bulletSpeed;
     private final double speedRotate;
     private final double damage;
     private final int range;
     private final List<TrunkInfo> gunTrunksInfo;
+    private final double bulletExplosionPower;
     private final String spriteName;
     private final String title;
     private final String type;
     private final int size;
+    private final Map<String, BulletInfo> bulletMapping;
 }
