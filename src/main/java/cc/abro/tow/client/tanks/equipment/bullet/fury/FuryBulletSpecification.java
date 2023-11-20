b5620db8465e324969d50d73257be4c57cdb4110
KeyJ148 <kejik148@gmail.com>
2023-11-20 17:00:06 +0300

    #191 Refactoring bullet

--- src/main/java/cc/abro/tow/client/tanks/equipment/bullet/fury/FuryBulletSpecification.java
+++ src/main/java/cc/abro/tow/client/tanks/equipment/bullet/fury/FuryBulletSpecification.java
@@ -3,11 +3,13 @@ package cc.abro.tow.client.tanks.equipment.bullet.fury;
 import cc.abro.tow.client.tanks.equipment.bullet.defaults.DefaultBulletSpecification;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
+@EqualsAndHashCode(callSuper = true)
 public class FuryBulletSpecification extends DefaultBulletSpecification {
     private final double minSpeed;
     private final double maxSpeed;
